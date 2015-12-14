0d6c345045a17d2ea876f3d4e85feed9c322ba9a
Julian Haupt <julian.haupt@hauptmedia.de>
2015-12-14 16:23:00 +0100

    added payment property to PaymentChangeResponseType

--- src/PHPCommerce/Vendor/Ratepay/Service/Payment/Type/Response/PaymentChangeResponseType.php
+++ src/PHPCommerce/Vendor/Ratepay/Service/Payment/Type/Response/PaymentChangeResponseType.php
@@ -1,5 +1,28 @@
 <?php
 namespace PHPCommerce\Vendor\Ratepay\Service\Payment\Type\Response;
+use JMS\Serializer\Annotation\Type;
 
 class PaymentChangeResponseType extends ResponseContentType {
+    /**
+     * @var PaymentResponseType
+     * @Type("PHPCommerce\Vendor\Ratepay\Service\Payment\Type\Response\PaymentResponseType")
+     */
+    protected $payment;
+
+    /**
+     * @return PaymentResponseType
+     */
+    public function getPayment()
+    {
+        return $this->payment;
+    }
+
+    /**
+     * @param PaymentResponseType $payment
+     */
+    public function setPayment($payment)
+    {
+        $this->payment = $payment;
+        return $this;
+    }
 }
