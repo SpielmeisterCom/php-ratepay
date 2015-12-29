0acbdd70134c7b19f4696a376869edc9337362ce
Julian Haupt <julian.haupt@hauptmedia.de>
2015-12-29 21:07:45 +0100

    added deviceToken in CustomerDeviceType

--- src/PHPCommerce/Vendor/Ratepay/Service/Payment/Type/CustomerDeviceType.php
+++ src/PHPCommerce/Vendor/Ratepay/Service/Payment/Type/CustomerDeviceType.php
@@ -1,7 +1,29 @@
 <?php
 namespace PHPCommerce\Vendor\Ratepay\Service\Payment\Type;
+use JMS\Serializer\Annotation\SerializedName;
 
 class CustomerDeviceType
 {
+    /**
+     * @SerializedName("device-token")
+     * @var string
+     */
+    protected $deviceToken;
 
+    /**
+     * @return string
+     */
+    public function getDeviceToken()
+    {
+        return $this->deviceToken;
+    }
+
+    /**
+     * @param string $deviceToken
+     */
+    public function setDeviceToken($deviceToken)
+    {
+        $this->deviceToken = $deviceToken;
+        return $this;
+    }
 }
